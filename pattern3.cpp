6d8ad4ff6c9e11b52280bb973991a6d525ed26fd
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2023-07-22 23:03:59 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int row =1; row <=n ; row++)
+    {
+       
+        for(int col=1; col<=row; col++)
+        {
+            char ch= 'A'+n + col-row-1;
+            cout<<ch<<" ";
+           
+        }
+        cout << endl;
+
+       
+    }
+}
