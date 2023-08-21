65450c5ca70934b3a2afbd2149e70e02a3260a28
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2023-08-21 18:59:34 +0530

    Create # 852 leetcode.cpp

---
+++ # 852 leetcode.cpp
@@ -0,0 +1,31 @@
+pak pak pakak
+pakak pak pak pak
+pak pak pak pak pak
+pakak pakak pakak
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int s=0;
+        int e = arr.size() - 1;
+
+        int mid= s + (e-s)/2;
+        
+
+        while(s<e)
+        {
+            if (arr[mid]> arr[mid+1] && arr[mid] > arr[mid-1])
+            return mid;
+
+            else if(arr[mid]< arr[mid+1])
+            s= mid+1;
+
+            else if (arr[mid]> arr[mid+1])
+            e = mid;
+
+            mid= s + (e-s)/2;
+        }
+        return -1;
+        
+    }
+};
