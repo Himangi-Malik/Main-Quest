a4e5bf1753807d7dce0027829cda8482afc621b8
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2023-08-29 17:42:30 +0530

    Create bubble sort.cpp

---
+++ coding ninja/bubble sort.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{ 
+    for(int i = 1; i<n; i++)  
+    {
+        for(int j=0; j<n-i; j++)
+        {
+            if(arr[j]> arr[j+1])
+            swap(arr[j], arr[j+1]);
+        }
+        
+    }
+
+}
