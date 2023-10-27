c6905c8beb5f41e1b76ac1e46a25a6d665085bf2
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2023-10-27 17:57:48 +0530

    Create sum of 2 array.cpp

---
+++ coding ninja/sum of 2 array.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <vector> 
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m)
+{
+	vector <int> ans;
+	int i = n-1;
+	int j = m-1;
+	int carry = 0;
+	iint sum= 0;
+	
+	while(i>=0 && j>=0)
+	{
+		sum = a[i] + b[j] + carry;
+		carry = sum / 10;
+		sum = sum % 10;
+
+		ans.push_back(sum);
+		i--; j--;
+	}
+	
+	while(i>=0)
+	{
+		sum = a[i]+ carry;
+		carry = sum/10;
+		sum = sum%10;
+		ans.push_back(sum);
+		i--;
+	}
+
+	while(j>=0)
+	{
+		sum = b[j]+ carry;
+		carry = sum/10;
+		sum = sum%10;
+		ans.push_back(sum);
+		j--;
+	}
+
+	while(carry!=0)
+	{
+		ans.push_back(carry);
+	}
+
+	reverse(ans.begin(), ans.end());
+	return ans;
+	
+
+
+}
