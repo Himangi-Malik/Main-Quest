431b755ae6f040f4bb3c5f815c048965f3d60b13
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2024-02-11 14:01:13 +0530

    split linked list in k parts

---
+++ LinkedList/#725 leetcode.cpp
@@ -0,0 +1,30 @@
+//samjh aagya tha kya karna, doosre type se answer nikaal bhi diya tha
+//but it was taking too much time and slowing me down so pokaik puk puk
+//i am skipping this but sahi socha tha maine remainder and dividend ke case mein thanks
+class Solution {
+public:
+    int length(ListNode* head){
+        int count = 0;
+        while(head!= NULL){
+            count++;
+            head = head->next
+        }
+        cout << length << endl;
+        return count;
+    }
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
+        vector <ListNode*> ans;//final ans ka vector
+
+        int full_length = length(head);
+        int div = full_length/k;
+        int rem = full_length % k;
+        int count, check = 0;
+
+        ListNode* curr = head; 
+        ListNode* prev = NULL;
+        
+    while()
+
+        
+    }
+};
