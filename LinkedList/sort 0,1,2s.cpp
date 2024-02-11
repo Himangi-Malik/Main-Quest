fe0c22a5f971e679156f87166b13abf7eae2f989
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2024-02-11 14:24:34 +0530

    Update sort 0,1,2s.cpp

--- LinkedList/sort 0,1,2s.cpp
+++ LinkedList/sort 0,1,2s.cpp
@@ -1,1 +1,29 @@
+//very easy sawal, i did very quickly
+Node* sortList(Node *head){
+    // Write your code here.
+    int arr[] = {0,0,0};
+    Node* temp = head;
+    while(temp!= NULL){
+        if(temp->data == 0)
+        arr[0]++;
 
+        else if(temp->data == 1)
+        arr[1]++;
+
+        else
+        arr[2]++;
+
+        temp = temp->next;
+    }
+
+    //ab allocation ka algo likhenge ham ache se thanks 
+    temp = head;
+    for (int i = 0; i < 3; i++) {
+        while(arr[i]!= 0){
+            temp->data = i;
+            arr[i]--;
+            temp = temp->next;
+        }
+    }
+    return head;
+}
