80b7b2eedc46b2312750537ac585a3f11e98d1da
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2024-02-11 15:32:01 +0530

    approach 2 using merging

--- LinkedList/sort 0,1,2s.cpp
+++ LinkedList/sort 0,1,2s.cpp
@@ -27,3 +27,68 @@ Node* sortList(Node *head){
     }
     return head;
 }
+
+
+
+
+//APPROACH 2
+void insertAtTail(Node* &tail, Node* &temp){
+    tail->next = temp;
+    tail = temp;
+}
+
+
+Node* sortList(Node *head){
+    // Write your code here.
+    // ab second approach try karenge ham ache se
+    Node* zeroHead = new Node(-1);//dummy node
+    Node* zeroTail = zeroHead;
+
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+    
+    Node* temp = head;
+ 
+    // separate lists are created
+    while(temp!= NULL){
+        if(temp-> data == 0)
+        {
+            insertAtTail(zeroTail, temp);
+        }
+
+        else if(temp-> data == 1)
+        {
+            insertAtTail(oneTail, temp);
+        }
+
+        else
+        {
+            insertAtTail(twoTail, temp);
+        }
+        temp = temp->next;
+    }
+
+    //merging the lists togther
+    if(oneHead->next!= NULL)
+    {
+        zeroTail->next = oneHead-> next;
+    }
+    else
+    zeroTail->next = twoHead->next;
+
+    oneTail->next = twoHead->next;
+    twoTail->next = NULL;
+
+    head = zeroHead->next;
+
+    delete zeroHead; 
+    delete oneHead; 
+    delete twoHead; 
+
+
+    return head;
+
+}
