ac9ecd77e2ba10341584cf91173daaa4de83c7c3
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2024-02-19 09:31:18 +0530

    Create valid parenthesis.cpp

---
+++ stacks/valid parenthesis.cpp
@@ -0,0 +1,42 @@
+bool isValidParenthesis(string s)
+{
+    // Write your code here.yes sure i will do the pleasure of writing the code here thanks
+    stack <char> st;
+    st.push(s[0]);
+    for(int i = 1; i< s.length();i++){
+        char ch = s[i];
+        
+        if(s[i] == ']')
+        {
+            if(st.top() == '[')
+            st.pop();
+        }
+
+        else if (s[i] == ')') {
+            if (st.top() == '(')
+            st.pop();
+        }
+
+        else if (s[i] == '}') {
+            if (st.top() == '{')
+            st.pop();
+        } else
+            st.push(ch);
+    }
+    //cout << st.top();
+   // st.pop();
+   // st.pop();
+    
+    //cout << st.top();
+    //cout << st.top();
+    //cout << st.top();
+        
+
+
+    if(st.empty())
+    return true;
+
+    else
+    return false;
+
+}
