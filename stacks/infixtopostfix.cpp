68e14d3676df67628220a235a02df48924ba4e25
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2024-04-01 22:35:35 +0530

    Update infixtopostfix.cpp

--- stacks/infixtopostfix.cpp
+++ stacks/infixtopostfix.cpp
@@ -1,60 +1,7 @@
-#include <stack>
-string infixToPostfix(string exp){
-	// Write your code here
-	stack <char> st;
-	string ans;
-	//int precedence = 0;
-
-	for(int i = 0; i<exp.length(); i++){
-		cout << exp << endl;
-		//cout << ans << endl;
-		cout << st.top();
-
-	
-		if(exp[i] == '('){
-			st.push(exp[i]);
-		}
-
-		else if(exp[i] == ')'){
-	
-			while(st.top() != '('){
-				ans.push_back(st.top());
-				st.pop();
-			}
-			st.pop(); //bacha hua ( yeh bhi pop ho jaaye)
-			
-		}
-
-		else if(exp[i] == '^'){
-		    st.push('^');
-		}
-
-		else if(exp[i]== '*' || '/'){
-	        while (st.top() != '^' ||  '*' || '/' ){
-				ans.push_back(st.top());
-				st.pop();
-			}
-			ans.push_back(exp[i]);
-
-		}
-
-		else if(exp[i] == '+' || '-'){
-			while (st.top() != '^' ||  '*' || '/' ||  '+' || '-'){
-				ans.push_back(st.top());
-				st.pop();
-			}
-			ans.push_back(exp[i]);
-
-
-		}
-		else
-		ans.push_back(exp[i]);  //operand
-
-		
-	}
-        while (!st.empty()) {
+		 else if (exp[i] == '^' || exp[i] == '*' || exp[i] == '/') {
+            while (!st.empty() && (st.top() == '^' || st.top() == '*' || st.top() == '/')) {
                 ans.push_back(st.top());
-                st.top();
-        }
-        return ans;
-}
+                st.pop();
+            }
+            st.push(exp[i]);
+        } 
